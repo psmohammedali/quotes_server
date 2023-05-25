047fa307e9515752d308dfce5c8d323e9311faca
Mohammed Ali P S <48435733+psmohammedali@users.noreply.github.com>
2023-05-25 22:43:39 +0530

    Update api_request.py

--- api_request.py
+++ api_request.py
@@ -2,7 +2,7 @@ import requests
 import json
 category = 'happiness'
 api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
-response = requests.get(api_url, headers={'X-Api-Key': 'GThz84Eh9jw9i/If7Ur0Rw==EixW0RCnd4OteSZY'})
+response = requests.get(api_url, headers={'X-Api-Key': ''})
 
 response = json.loads(response.text)
 print(response[0]["quote"])
